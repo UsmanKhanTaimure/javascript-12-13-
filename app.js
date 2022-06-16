2083324259f1f7e0dd9650716e958e99f81b5603
UsmanKhanTaimure <107642292+UsmanKhanTaimure@users.noreply.github.com>
2022-06-16 11:01:42 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// Chapter 12 to 13
+// Question 1;
+// var user_input = prompt("Enter Your Lower case And Upper Case Number");
+// if (user_input>=65 && user_input<=90){
+//     alert("AlphaBetes number")
+// }else if(user_input>=97 && user_input<=122){
+//     alert("Lower Alphabets")
+// }
+
+
+// Question2
+// var inp1 = +prompt("Enter Your Number1");
+// var inp2 = +prompt("Enter Your Number2");
+// if (inp1>inp2 || inp2>inp1){
+//     alert("Inp1 Greter than Inp2")
+// }else if(inp1===inp2){
+//     alert("Inp1 equal to Inp2")
+// }else{
+//     alert("Inp1 Smaller to Inp2")
+// }
+
+
+// Question3
+// var user_input = +prompt("Enter Your Number");
+// if (user_input>0){
+//     alert("Your Number is positive")
+// }else if(user_input<0){
+//     alert("Your Number is Negetive")
+// }else if (user_input===0){
+//     alert("Numer is zero")
+// }
+
+
+// Question4 
+// var vowel = prompt("Enter Your vowel number");
+// if (vowel=="a" || vowel=="e" || vowel=="i" || vowel=="o" || vowel=="u"){
+//     alert("This is Vowel Number")
+// }else{
+//     alert("This is no vowel number")
+// }
+
+
+// Question5
+// var input_password = prompt("Enter your password");
+// var password = "usman123";
+// if (input_password == false){
+//     alert("Please enter your password")
+// }else if (input_password===password){
+//     alert("Correct! The password you entered matches the original password")
+// }else{
+//     alert("Incorrect password")
+// }
+
+
+// Question6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// document.write(greeting)
+// }else{
+// greeting = "Good evening";
+// document.write(greeting)
+// }
+
+
+// Question7
+// var user_input = +prompt("Enter your time 0000 and 2359");
+// if (user_input>=0000 && user_input<1200){
+//     alert("Good Morning")
+// }else if(user_input>=1200 && user_input<1700){
+//     alert("Good Afternoon")
+// }else if(user_input>=1700 && user_input<2100){
+//     alert("Good evening")
+// }else if(user_input>=2100 && user_input<2359){
+//     alert("Good night")
+// }
